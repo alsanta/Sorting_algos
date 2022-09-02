4293823ab5fb86df1c4ac3c6ae99ca2f254f92f0
Alex Santana <alexandresntn@yahoo.com>
2022-09-02 16:24:32 -0400

    main

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <h1>Live Algos</h1>
+      </div>
+      <Switch>
+        
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
